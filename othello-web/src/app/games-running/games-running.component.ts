3cd76990c482113f7053197b4043b6d245bf5c83
raqueldj <raquel.diasjales@gmail.com>
2017-04-11 15:12:35 +0200

    menus simples

--- othello-web/src/app/games-running/games-running.component.ts
+++ othello-web/src/app/games-running/games-running.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import { Game } from './games'
 
 @Component({
   selector: 'app-games-running',
   templateUrl: './games-running.component.html',
   styleUrls: ['./games-running.component.css']
 })
-export class GamesRunningComponent implements OnInit {
+export class GamesRunningComponent {
+  @Input() games;
 
-  constructor() { }
+  @Output() createGame: EventEmitter<null> = new EventEmitter();
 
-  ngOnInit() {
+  gamesList: Game[] = [];
+
+  Id = 1;
+  addGame(player1, player2) {
+    this.gamesList.push({
+      id: this.Id++,
+      player1,
+      player2
+    })
   }
 
+  removeGame(gameIndex) {
+    this.gamesList.splice(gameIndex, 1);
+  }
+
+
+  onNewGame() {
+    this.createGame.emit();
+  }
 }
