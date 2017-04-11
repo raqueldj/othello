6158c55943ff5bbdbd7a9e1616ceb733f43eec94
cleymobil <clement.coutey@hotmail.fr>
2017-04-11 15:08:58 +0200

    UserWs

--- othello/src/main/java/io/avengers/othello/ws/UserWs.java
+++ othello/src/main/java/io/avengers/othello/ws/UserWs.java
@@ -1,12 +1,16 @@
 package io.avengers.othello.ws;
 
+import java.util.List;
+
 import javax.ejb.EJB;
-import javax.websocket.server.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -20,6 +24,17 @@ public class UserWs {
 	
 	@EJB
 	UserService service;
+	
+	@GET
+	public List<User> findAll(){
+		return service.findAll();
+	}
+	
+	@GET
+	@Path("{id}")
+	public User findById(@PathParam("id") int id){
+		return service.findById(id).orElseThrow(NotFoundException::new);
+	}
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -38,4 +53,15 @@ public class UserWs {
 		return service.updateUser(id, user);
 	}
 	
+	@PUT
+	@Path("/{id}/win")
+	public void updateUserWin(@PathParam("id") int id){
+		service.updateUserWin(id);
+	}
+	
+	@PUT
+	@Path("/{id}/lose")
+	public void updateUserLose(@PathParam("id") int id){
+		service.updateUserLose(id);
+	}
 }
