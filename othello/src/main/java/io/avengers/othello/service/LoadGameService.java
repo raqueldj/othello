8fb7eb9c7f806cd813a2dd1bab14c0d4bbe29537
raqueldj <raquel.diasjales@gmail.com>
2017-04-12 15:35:07 +0200

    get all games running

--- othello/src/main/java/io/avengers/othello/service/LoadGameService.java
+++ othello/src/main/java/io/avengers/othello/service/LoadGameService.java
@@ -1,5 +1,48 @@
 package io.avengers.othello.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import io.avengers.othello.dao.GameDao;
+import io.avengers.othello.domain.Game;
+import io.avengers.othello.dto.GameDto;
+
+@Stateless
+@Named
 public class LoadGameService {
 
+	GameDao gameDao;
+
+	@PersistenceContext
+	EntityManager em;
+
+	@PostConstruct
+	void after() {
+		System.out.println("----------PostConstruct in FunkoPopService----------");
+		this.gameDao = new GameDao(em);
+	}
+
+	public List<GameDto> findAllGamesRunning() {
+		List<Game> games = gameDao.findRunning();
+		List<GameDto> gamesDto = new ArrayList<>();
+		GameDto gameDto = new GameDto();
+
+		for (Game game : games) {
+
+			gameDto.setId(game.getId());
+			gameDto.getPlayerBlack().setId(game.getBlackUser().getId());
+			gameDto.getPlayerBlack().setName(game.getBlackUser().getName());
+			gameDto.getPlayerWhite().setId(game.getWhiteUser().getId());
+			gameDto.getPlayerWhite().setName(game.getWhiteUser().getName());
+
+			gamesDto.add(gameDto);
+		}
+		return gamesDto;
+	}
 }
--- othello/src/main/java/io/avengers/othello/ws/LoadGameWs.java
+++ othello/src/main/java/io/avengers/othello/ws/LoadGameWs.java
@@ -1,5 +1,27 @@
 package io.avengers.othello.ws;
 
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import io.avengers.othello.dto.GameDto;
+import io.avengers.othello.service.LoadGameService;
+
+@Path("load-game")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes("application/json")
 public class LoadGameWs {
 
+	@EJB
+	LoadGameService loadGameService;
+	
+	@GET
+	public List<GameDto> findAllGamesRunning(){
+		return loadGameService.findAllGamesRunning();
+	}
 }
