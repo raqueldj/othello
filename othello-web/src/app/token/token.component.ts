9c6aa7573cc7fd1086e9089d84a0ed1e02a27f55
raqueldj <raquel.diasjales@gmail.com>
2017-04-12 14:13:59 +0200

    load

--- othello-web/src/app/token/token.component.ts
+++ othello-web/src/app/token/token.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+
+import { Token } from '../modele';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-token',
   templateUrl: './token.component.html',
-  styleUrls: ['./token.component.css']
+  styleUrls: ['./token.component.css'],
+  providers: [DataService]
 })
-export class TokenComponent implements OnInit {
+export class TokenComponent {
+  @Input() token;
+  @Output() position: EventEmitter<null> = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private dataService: DataService) {
   }
 
+tokenPosition: Token[] = this.dataService.tokenPosition;
 }
