53e8b58403b2f99caf3b6e21f0e0622f4fd7b13e
cleymobil <clement.coutey@hotmail.fr>
2017-04-13 12:13:59 +0200

    GameService

--- othello/src/main/java/io/avengers/othello/dto/TokenCreatedDto.java
+++ othello/src/main/java/io/avengers/othello/dto/TokenCreatedDto.java
@@ -4,5 +4,19 @@ public class TokenCreatedDto {
 	
 	int tokenId;
 	boolean hasBeenCreated;
+	public int getTokenId() {
+		return tokenId;
+	}
+	public void setTokenId(int tokenId) {
+		this.tokenId = tokenId;
+	}
+	public boolean isHasBeenCreated() {
+		return hasBeenCreated;
+	}
+	public void setHasBeenCreated(boolean hasBeenCreated) {
+		this.hasBeenCreated = hasBeenCreated;
+	}
+	
+	
 
 }
